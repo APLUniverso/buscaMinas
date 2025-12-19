759d411f1043accae68b8e0e33316e368459af31
APLUniverso <juanfelixbd@gmail.com>
2025-12-19 15:32:31 -0500

    fix: :bug: Cambio de dimensiones
    si cambiaba las dimesiones el codigo no creaba las minas para todo el tablero

--- app.py
+++ app.py
@@ -55,23 +55,21 @@ def imprimirTablero(tablero):
     for linea in tablero:
         print(" ".join(linea))
 
+def  crearMinas(dimensiones):
+    minas = []
+    #Se crean las cordenadas de la minas
+    for i in range(0,num_minas):
+        mina = [random.randint(0,dimensiones[0]-1),random.randint(0,dimensiones[1]-1)] # mina random del 1 al 99 pa este caso
+        if mina not in minas: # Verificamos que la mina no se repita
+            minas.append(mina)
+        else: # Si la mina se repite le restamos uno al contador
+            i -= 1
+    return minas
 
-dimensiones = [10,10]
-num_minas = 20
+dimensiones = [20,10]
+num_minas = 30
 tablero = []
-minas = []
-
-#Se crean las cordenadas de la minas
-for i in range(0,num_minas):
-    mina = random.randint(0,(dimensiones[0]*dimensiones[1])-1) # mina random del 1 al 99 pa este caso
-    if mina not in minas: # Verificamos que la mina no se repita
-        if mina < 10: # si la mina es de un digito el x es 0
-            minas.append([0,mina])
-        else: # si no pues el primer digito es la X y el segundo es la Y
-            minas.append([int(mina/10),mina-int(mina/10)*10])
-    else: # Si la mina se repite le restamos uno al contador
-        i -= 1
-
+minas = crearMinas(dimensiones)
 
 imprimirTablero(crearTablero(dimensiones,minas))
 
