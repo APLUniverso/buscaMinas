690b17b2b4b69d6ca7401e81c6134359dfb5d1b5
APLUniverso <juanfelixbd@gmail.com>
2025-12-19 14:49:59 -0500

    feat: :sparkles: Funciones
    se crean las funciones encargadas de crear e imprimir el tablero del busca minas

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 # se crea un generador del tablero tradicional del busca minas
 import random
 
+def numMinasAlrededor(cordenada,minas):
+    x = cordenada[0]
+    y = cordenada[1]
+    perimetroCordendas = [
+        [x-1,y],# Cordenada de arriba
+        [x+1,y],# Cordenada de abajo
+        [x,y-1],# Cordenada de derecha
+        [x,y+1],# Cordenada de izquierda
+        # DIAGONALES
+        [x-1,y+1],# Cordenada de arriba derecha
+        [x-1,y-1],# Cordenada de arriba izquierda
+        [x+1,y+1],# Cordenada de abajo derecha
+        [x+1,y-1]# Cordenada de abajo izquierda
+    ]
+    num = 0
+    for perimetro in perimetroCordendas:
+        if perimetro in minas:
+            num += 1
+    return num
+
+def crearTablero(dimensiones, minas):
+    tablero = []
+    for x in range(dimensiones[0]):
+        linea = []
+        for y in range(dimensiones[1]):
+            if [x,y] in minas:
+                linea.append("X")
+            else:
+                num = numMinasAlrededor([x,y],minas)
+                linea.append(num)
+        tablero.append(linea)
+    return tablero
+
+def imprimirTablero(tablero):
+    for linea in tablero:
+        print("".join(linea)+"\n")
+
+
 dimensiones = [10,10]
 num_minas = 10
 tablero = []
@@ -16,6 +54,7 @@ for i in range(0,10):
             minas.append([int(mina/10),mina-int(mina/10)*10])
     else: # Si la mina se repite le restamos uno al contador
         i -= 1
-    
 
+
+imprimirTablero(crearTablero(dimensiones,minas))
 
